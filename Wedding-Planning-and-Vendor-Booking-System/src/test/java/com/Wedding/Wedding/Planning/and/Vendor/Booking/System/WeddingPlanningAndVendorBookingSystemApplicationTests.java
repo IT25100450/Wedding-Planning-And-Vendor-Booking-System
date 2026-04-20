d0b4b05b76e4d6e9c09f9dda4c786f4d4e125e37
IT25100450 <it25100450@my.sliit.lk>
2026-04-20 07:43:10 +0530

    Springboot-Framework

---
+++ Wedding-Planning-and-Vendor-Booking-System/src/test/java/com/Wedding/Wedding/Planning/and/Vendor/Booking/System/WeddingPlanningAndVendorBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wedding.Wedding.Planning.and.Vendor.Booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPlanningAndVendorBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
