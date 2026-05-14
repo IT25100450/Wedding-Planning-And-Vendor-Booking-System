6236f48c35e5da4903eba76435c707a8e0914b8b
IT25100928 <IT25100928@my.sliit.lk>
2026-05-15 00:06:40 +0530

    fixed spring boot

---
+++ EternalBliss/src/main/java/com/EternalBliss/EternalBliss/EternalBlissApplication.java
@@ -0,0 +1,13 @@
+package com.EternalBliss.EternalBliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalBlissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalBlissApplication.class, args);
+	}
+
+}
