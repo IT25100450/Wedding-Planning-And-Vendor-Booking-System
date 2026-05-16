12969abe6944d452400c7248d545117df6106f9f
IT25102279 <IT25102279@my.sliit.lk>
2026-05-16 18:43:37 +0530

    Vendor-repository

---
+++ src/main/java/com/EternalBliss/EternalBliss/repository/vendorrepo.java
@@ -0,0 +1,8 @@
+package com.EternalBliss.EternalBliss.repository;
+
+import com.EternalBliss.EternalBliss.model.vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface vendorrepo extends JpaRepository<vendor, Integer> {
+
+}
