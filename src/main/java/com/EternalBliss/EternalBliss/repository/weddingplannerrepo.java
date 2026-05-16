38ee757c93dd5342adbf07ef844f2ebb79bc30e4
IT25100928 <IT25100928@my.sliit.lk>
2026-05-16 22:14:04 +0530

    weddingchecklist-repository

---
+++ src/main/java/com/EternalBliss/EternalBliss/repository/weddingplannerrepo.java
@@ -0,0 +1,8 @@
+package com.EternalBliss.EternalBliss.repository;
+
+import com.EternalBliss.EternalBliss.model.weddingplanner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface weddingplannerrepo extends JpaRepository<weddingplanner, Integer> {
+
+}
