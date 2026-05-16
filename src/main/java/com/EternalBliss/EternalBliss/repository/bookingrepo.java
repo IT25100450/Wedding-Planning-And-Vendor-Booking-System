96841c92f1c837ccbfbe68106e81dce10750a276
IT25101875 <it25101875@my.sliit.lk>
2026-05-16 20:57:13 +0530

    Booking-repository

---
+++ src/main/java/com/EternalBliss/EternalBliss/repository/bookingrepo.java
@@ -0,0 +1,7 @@
+package com.EternalBliss.EternalBliss.repository;
+
+import com.EternalBliss.EternalBliss.model.booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface bookingrepo extends JpaRepository<booking, Long> {
+}
