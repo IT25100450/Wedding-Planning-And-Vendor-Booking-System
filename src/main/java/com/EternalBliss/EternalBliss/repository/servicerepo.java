238803e82ebab524727a8726bc90156c56d70429
IT25103172 <it25103172@my.sliit.lk>
2026-05-16 16:42:17 +0530

    service-repository

---
+++ src/main/java/com/EternalBliss/EternalBliss/repository/servicerepo.java
@@ -0,0 +1,7 @@
+package com.EternalBliss.EternalBliss.repository;
+
+import com.EternalBliss.EternalBliss.model.service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface servicerepo extends JpaRepository<service, Integer> {
+}
